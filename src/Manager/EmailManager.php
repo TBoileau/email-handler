b47f1d633b250812bc02610e45c29e9c04480e4b
Thomas Boileau <t-boileau@email.com>
2018-12-31 01:21:31 +0100

    Update EmailManager.php

--- src/Manager/EmailManager.php
+++ src/Manager/EmailManager.php
@@ -7,36 +7,6 @@ use TBoileau\EmailHandlerBundle\Email\EmailInterface;
 /**
  * Class EmailManager
  * @package TBoileau\EmailHandlerBundle\Manager
- * @method EmailManagerInterface addPart($body, $contentType = null, $chartset = null)
- * @method EmailManagerInterface attachSigner(\Swift_Signer $signer)
- * @method EmailManagerInterface detachSigner(\Swift_Signer $signer)
- * @method EmailManagerInterface clearSigners()
- * @method EmailManagerInterface toByteStream(\Swift_InputByteStream $is)
- * @method EmailManagerInterface doSign()
- * @method EmailManagerInterface saveMessage()
- * @method EmailManagerInterface saveHeaders(array $altered)
- * @method EmailManagerInterface restoreHeaders()
- * @method EmailManagerInterface restoreMessage()
- * @method EmailManagerInterface setSubject($subject)
- * @method EmailManagerInterface setDate(DateTimeInterface $dateTime)
- * @method EmailManagerInterface setReturnPath($address)
- * @method EmailManagerInterface getReturnPath()
- * @method EmailManagerInterface setSender($address, $name = null)
- * @method EmailManagerInterface addFrom($address, $name = null)
- * @method EmailManagerInterface setFrom($addresses, $name = null)
- * @method EmailManagerInterface addReplyTo($address, $name = null)
- * @method EmailManagerInterface setReplyTo($addresses, $name = null)
- * @method EmailManagerInterface addTo($address, $name = null)
- * @method EmailManagerInterface setTo($addresses, $name = null)
- * @method EmailManagerInterface addCc($address, $name = null)
- * @method EmailManagerInterface setCc($addresses, $name = null)
- * @method EmailManagerInterface addBcc($address, $name = null)
- * @method EmailManagerInterface setBcc($addresses, $name = null)
- * @method EmailManagerInterface setPriority($priority)
- * @method EmailManagerInterface setReadReceiptTo($addresses)
- * @method EmailManagerInterface attach(\Swift_Mime_SimpleMimeEntity $entity)
- * @method EmailManagerInterface detach(\Swift_Mime_SimpleMimeEntity $entity)
- * @method EmailManagerInterface embed(\Swift_Mime_SimpleMimeEntity $entity)
  */
 class EmailManager implements EmailManagerInterface
 {
@@ -103,6 +73,7 @@ class EmailManager implements EmailManagerInterface
      */
     public function send(): int
     {
+        $this->email->buildEmail($this);
         return $this->mailer->send($this->message);
     }
 
